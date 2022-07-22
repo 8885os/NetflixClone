e7621dbfbdc22ca6ea04a749ddbbebe18e96dae1
8885os <d729258@gmail.com>
2022-07-22 23:22:43 +0100

    text align

--- src/components/Loggedin/Home.js
+++ src/components/Loggedin/Home.js
@@ -37,7 +37,7 @@ const Home = ({ setComponent, movieSetter, showSetter }) => {
             })
 
 
-    }, [])
+    }, [movies, shows, topRatedMovie, topRatedShow])
 
     useEffect(() => {
         setMix([])
